31a72e8b22926ff7dcf5fff639d81252b44fee32
chenqiang <1252321427@qq.com>
2021-01-07 18:45:55 +0800

    ''

--- webhook.js
+++ webhook.js
@@ -17,9 +17,11 @@ app.post("/webhook", function (req, res) {
   console.log("方法和地址", req.method, req.url);
   let buffers = [];
   req.on("data", (buffer) => {
+    console.log("开始接收流");
     buffers.push(buffer);
   });
   res.on("end", () => {
+    console.log("流接收结束");
     let body = Buffer.concat(buffers);
     let event = req.headers["x-github-event"]; // 类型 push
     let signature = req.headers["x-hub-signature"]; // 签名采用hash算法 github请求来的时候，要传递请求体body，还会传来一个signature过来，你需要验证签名是否有效
