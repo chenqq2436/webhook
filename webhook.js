714af3283c95e1392c1ef366edb80a19e1737aaa
chenqiang <1252321427@qq.com>
2021-01-08 10:37:53 +0800

    ''

--- webhook.js
+++ webhook.js
@@ -20,22 +20,20 @@ const app = express();
 
 // 接受githubpush的hook
 app.post("/webhook", function (req, res) {
-  console.log("方法和地址", req.method, req.url);
+  console.log("------方法和地址------", req.method, req.url);
   let buffers = [];
   req.on("data", (buffer) => {
-    console.log("开始接收流");
+    console.log("------开始接收流------");
     buffers.push(buffer);
   });
-  req.on("error", (err) => {
-    console.log("接收流错误了", err);
-  });
   req.on("end", () => {
-    console.log("流接收结束");
+    console.log("------流接收结束-----");
     let body = Buffer.concat(buffers);
     let event = req.headers["x-github-event"]; // 类型 push
     let signature = req.headers["x-hub-signature"]; // 签名采用hash算法 github请求来的时候，要传递请求体body，还会传来一个signature过来，你需要验证签名是否有效
     // 判断签名是否合法
     if (signature !== sign(body)) {
+      console.log("-------签名信息不合法-----");
       return res.end("Not Allowed");
     }
     res.setHeader("Content-Type", "application/json");
@@ -44,8 +42,9 @@ app.post("/webhook", function (req, res) {
     if (event == "push") {
       // 拿到推送信息
       let payload = JSON.parse(body);
-      console.log("------参数信息-----");
+      console.log("------参数信息开始-----");
       console.log(payload);
+      console.log("------参数信息结束-----");
       // 开启子进程去执行对应的脚本 目前通过仓库名称匹配
       const child = spawn("sh", [`./${payload.repository.name}.sh`]);
       const logBuffers = [];
@@ -57,6 +56,9 @@ app.post("/webhook", function (req, res) {
         console.log("--------日志信息-------", log.toString());
       });
     }
+  });
+  req.on("error", (err) => {
+    console.log("接收流错误了", err);
   });
 });
 
